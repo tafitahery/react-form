e2878c2f3524e65ade8f3edce439ff12196a1cb1
Tafita Hery <tafitahery@gmail.com>
2022-12-09 06:18:48 +0300

    How to Prevent Re-rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import './app.css';
 import FormInput from './components/FormInput';
 
 function App() {
-  const [username, setUsername] = useState('');
+  // const [username, setUsername] = useState('');
 
-  console.log(username);
+  console.log('re-rendered');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    console.log(Object.fromEntries(data.entries()));
+  };
 
   return (
     <div className="app">
-      <form>
-        <FormInput placeholder="Username" setUsername={setUsername} />
-        <FormInput placeholder="Email" />
-        <FormInput placeholder="Full Name" />
-        <FormInput placeholder="Sth else" />
+      <form onSubmit={handleSubmit}>
+        <FormInput name="username" placeholder="Username" />
+        <FormInput name="email" placeholder="Email" />
+        <FormInput name="fullame" placeholder="Full Name" />
+        <FormInput name="sth" placeholder="Sth else" />
+        <button>Submit</button>
       </form>
     </div>
   );
--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -4,10 +4,7 @@ export default function FormInput(props) {
   return (
     <div className="formInput">
       {/* <label>Username</label> */}
-      <input
-        placeholder={props.placeholder}
-        onChange={(e) => props.setUsername(e.target.value)}
-      />
+      <input name={props.name} placeholder={props.placeholder} />
     </div>
   );
 }
