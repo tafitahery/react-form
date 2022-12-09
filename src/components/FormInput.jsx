03f6ab55c8e73531415bf6eac3d6abe012a3602c
Tafita Hery <tafitahery@gmail.com>
2022-12-09 19:43:07 +0300

    React Form Input Validation and Displaying Errors

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,12 +1,28 @@
+import { useState } from 'react';
 import './formInput.css';
 
 export default function FormInput(props) {
-  const { label, onChange, id, ...inputProps } = props;
+  const [focused, setFocused] = useState(false);
+
+  const { label, onChange, errorMessage, id, ...inputProps } = props;
+
+  const handleFocus = (e) => {
+    setFocused(true);
+  };
 
   return (
     <div className="formInput">
       <label>{label}</label>
-      <input {...inputProps} onChange={onChange} />
+      <input
+        {...inputProps}
+        onChange={onChange}
+        onBlur={handleFocus}
+        onFocus={() =>
+          inputProps.name === 'confirmPassword' && setFocused(true)
+        }
+        focused={focused.toString()}
+      />
+      <span>{errorMessage}</span>
     </div>
   );
 }
